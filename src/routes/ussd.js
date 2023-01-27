16c7ca9169f0d3b7f057166685d1a2ac12018932
JoelWekesa <joelwekesa.jw@gmail.com>
2023-01-27 17:01:03 +0300

    Changes

--- src/routes/ussd.js
+++ src/routes/ussd.js
@@ -1,9 +1,14 @@
 const ussdApi = (req, res) => {
+	let response;
 	console.log(res.body);
 
 	if (text === '') {
 		response = 'CON Enter your name';
 	}
+
+	setTimeout(() => {
+		res.send(response);
+	}, res.end);
 };
 
 module.exports = {
