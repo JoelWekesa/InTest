c30485e97de144ff5f73b90390ee1c1cfc4a3d13
JoelWekesa <joelwekesa.jw@gmail.com>
2023-01-28 07:17:06 +0300

    Changes

--- src/helpers/status.js
+++ src/helpers/status.js
@@ -1,4 +1,17 @@
-const status = (cover) => {
+const { Cover } = require('../models/cover');
+
+const status = async (cover) => {
+	const n = await Cover.findOne({ cover })
+		.then((data) => {
+			if (!data) {
+				return 'Invalid policy number';
+			}
+
+			return `Your policy number ${cover} is ${data.status}`;
+		})
+		.catch((err) => {
+			return 'An error occurred';
+		});
 	return cover;
 };
 
