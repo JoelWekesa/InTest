2d3e20a1fb01ff54c0d5f74603b071e389faf5dd
JoelWekesa <joelwekesa.jw@gmail.com>
2023-01-28 06:48:15 +0300

    Changes

--- src/routes/ussd.js
+++ src/routes/ussd.js
@@ -17,75 +17,9 @@ const ussdApi = (req, res) => {
 		// Business logic for first level response
 		response = `CON Enter Policy Number
         `;
-	} else if (('' + text).startsWith('1*')) {
-		// Business logic for first level response
-		// This is a terminal request. Note how we start the response with END
-		const arr = text.split('*');
-
-		const cover = arr[1];
-
-		async () => {
-			const status = await Cover.findOne({
-				cover,
-			}).then((data) => {
-				if (!data) {
-					response = `END Could not find a cover with policy number ${cover}`;
-				}
-				response = `END Your cover is ${data.status}`;
-			});
-
-			return status;
-		};
-	} else if (text === '1*1') {
-		// This is a second level response where the user selected 1 in the first instance
-		const nationalID = '12345678';
-		const policyNo = 'ACB1234KLM';
-		// This is a terminal request. Note how we start the response with END
-		response = `END Your cover of policy ${policyNo} associated with ID ${nationalID} is active`;
-	} else if (text === '1*2') {
-		// This is a second level response where the user selected 1 in the first instance
-		const nationalID = '12345678';
-		const policyNo = 'ACB1234KLM';
-		// This is a terminal request. Note how we start the response with END
-		response = `END Your cover of policy ${policyNo} associated with ID ${nationalID} is active`;
-	} else if (text === '2*1') {
-		response = `CON Select Type of Claim
-        1. Accident Claim
-        2. Natural Calamity Claim
-        3. Riot Claim`;
-	} else if (text === '2*1*1') {
-		response = `END You have successfully filled an accident claim.
-        One of our respondents will reach out for further details.
-        `;
-	} else if (text === '2*1*2') {
-		response = `END You have successfully filled a natural calamity claim.
-        One of our respondents will reach out for further details.
-        `;
-	} else if (text === '2*1*3') {
-		response = `END You have successfully filled a natural calamity claim.
-        One of our respondents will reach out for further details.
-        `;
-	} else if (text === '2*2') {
-		response = `CON Select Type of Claim
-        1. Accident Claim
-        2. Natural Calamity Claim
-        3. Riot Claim`;
-	} else if (text === '2*2*1') {
-		response = `END You have successfully filled an accident claim.
-        One of our respondents will reach out for further details.
-        `;
-	} else if (text === '2*2*2') {
-		response = `END You have successfully filled a natural calamity claim.
-        One of our respondents will reach out for further details.
-        `;
-	} else if (text === '2*2*3') {
-		response = `END You have successfully filled a natural calamity claim.
-        One of our respondents will reach out for further details.
-        `;
-
-		console.log('text', text);
+	} else {
+		response = `END ${text}`;
 	}
-
 	// Print the response onto the page so that our SDK can read it
 	res.set('Content-Type: text/plain');
 	res.send(response);
